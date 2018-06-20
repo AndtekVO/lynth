d07f117d1b37b6e7e4aeb6d1fdd8b562c134eaa4
ianandtek <ian.andtek@gmail.com>
2018-06-20 02:24:27 -0400

    update models

--- models.js
+++ models.js
@@ -1,16 +1,16 @@
 const ASCII = ['\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~'];
 
 var models = {
-    "shakespeare-250-250": {
+    "music-raw-250-250": {
         chars: ASCII,
         Ns: 250,
         Ni: 96,
         layers: 2,
-        path: 'models/shakespeare-250-250',
-        name: "William Shakespeare",
-        prefix: "My parts for thy face for me to alone\nThat such seat a subjects I have said.\n\nCLAUDIUS:\n"
+        path: 'models/music-raw-250-250',
+        name: "Pop Music",
+        prefix: 'He\'s the reason for the teardrops on my guitar\nThe only thing that keeps me wishing on a wishing star\n\n\n'
     },
-
+    
     "swift2-250-250": {
         chars: ASCII,
         Ns: 250,
@@ -31,8 +31,6 @@ var models = {
         prefix: 'He\'s the reason for the teardrops on my guitar\nThe only thing that keeps me wishing on a wishing star\n'
     },
 
-
-
     "tupac2-250-250": {
         chars: ASCII,
         Ns: 250,
@@ -42,5 +40,13 @@ var models = {
         name: "Tupac Shakur",
         prefix: 'He\'s the reason for the teardrops on my guitar\nThe only thing that keeps me wishing on a wishing star\n'
     },
-
+    "shakespeare-250-250": {
+        chars: ASCII,
+        Ns: 250,
+        Ni: 96,
+        layers: 2,
+        path: 'models/shakespeare-250-250',
+        name: "William Shakespeare",
+        prefix: "My parts for thy face for me to alone\nThat such seat a subjects I have said.\n\nCLAUDIUS:\n"
+    }
 }
